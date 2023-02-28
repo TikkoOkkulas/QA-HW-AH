5c1164f49808090f3d0b632f9791d101016a9c2c
Tikko <nort326@gmail.com>
2023-02-28 05:12:48 +0200

    HW_16 +updates

--- HW_16/Package2/tests2.py
+++ HW_16/Package2/tests2.py
@@ -1,14 +1,8 @@
 
-'''В другому пакунку створити файл з 3 параметризованими тестами, один з яких буде ітеруватись і перевіряти одиничні
-значення, другий перевірятиме пари значень, третій перевірятиме будь-які значення на ваш розсуд,
-але кожному значенню має бути присвоєний псевдонім. На кожен з тестів має бути застосована одна глобальна фікстура.
-На усіх тестах має бути мітка "param"'''
-
-
 import pytest
 
-uniq = ['Radament', 'Dark Elder', 'Ismail', 'Geleb', 'Fangskin', 'Andy', 'Duriq'] #list for tests 1 and 3
 
+uniq = ['Radament', 'Dark Elder', 'Ismail', 'Geleb', 'Fangskin', 'Andy', 'Duriq'] #list for tests 1 and 3
 @pytest.mark.param
 @pytest.mark.parametrize('council_member', ['Ismail', 'Geleb'])
 def test_one(council_member):
