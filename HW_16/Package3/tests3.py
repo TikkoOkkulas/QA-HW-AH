8c50f38362fc0aab10f5479956c3d3f29431b0eb
Tikko <nort326@gmail.com>
2023-02-28 05:14:48 +0200

    HW_16 +updates

--- HW_16/Package3/tests3.py
+++ HW_16/Package3/tests3.py
@@ -1,4 +1,5 @@
 
+
 import pytest
 
 @pytest.mark.pack
@@ -30,3 +31,8 @@ def test_four(func_fixture):
 def test_five(func_fixture):
     print('\nrun test 5')
     pass
+
+'''for terminal
+pytest HW_16/Package3/tests3.py -m pack
+pytest HW_16/Package3/tests3.py -m joint
+pytest HW_16/Package3/tests3.py -m rest'''
